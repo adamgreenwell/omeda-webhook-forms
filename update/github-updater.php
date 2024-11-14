053ac3cdcc5b77db04bc73203d3c6626c6a0ebf0
Adam Greenwell <adamgreenwell@gmail.com>
2024-11-14 11:39:00 -0500

    Housekeeping

--- update/github-updater.php
+++ update/github-updater.php
@@ -1,109 +1,117 @@
 <?php
-class GitHub_Updater {
-    private $slug;
-    private $plugin_data;
-    private $username;
-    private $repo;
-    private $plugin_file;
-    private $github_response;
 
-    public function __construct($plugin_file) {
-        $this->plugin_file = $plugin_file;
-        $this->slug = plugin_basename($plugin_file);
+class GitHub_Updater
+{
+	private $slug;
+	private $plugin_data;
+	private $username;
+	private $repo;
+	private $plugin_file;
+	private $github_response;
 
-        add_filter('pre_set_site_transient_update_plugins', [$this, 'modify_transient'], 10, 1);
-        add_filter('plugins_api', [$this, 'plugin_popup'], 10, 3);
-        add_filter('upgrader_post_install', [$this, 'after_install'], 10, 3);
-    }
+	public function __construct($plugin_file)
+	{
+		$this->plugin_file = $plugin_file;
+		$this->slug = plugin_basename($plugin_file);
 
-    public function set_github_info($username, $repo) {
-        $this->username = $username;
-        $this->repo = $repo;
-    }
+		add_filter('pre_set_site_transient_update_plugins', [$this, 'modify_transient'], 10, 1);
+		add_filter('plugins_api', [$this, 'plugin_popup'], 10, 3);
+		add_filter('upgrader_post_install', [$this, 'after_install'], 10, 3);
+	}
 
-    private function get_repository_info() {
-        if (!empty($this->github_response)) {
-            return;
-        }
+	public function set_github_info($username, $repo)
+	{
+		$this->username = $username;
+		$this->repo = $repo;
+	}
 
-        $request_uri = sprintf('https://api.github.com/repos/%s/%s/releases/latest',
-            $this->username, $this->repo);
+	private function get_repository_info()
+	{
+		if (!empty($this->github_response)) {
+			return;
+		}
 
-        $response = json_decode(wp_remote_retrieve_body(wp_remote_get($request_uri)));
+		$request_uri = sprintf('https://api.github.com/repos/%s/%s/releases/latest',
+			$this->username, $this->repo);
 
-        if (is_array($response)) {
-            return false;
-        }
+		$response = json_decode(wp_remote_retrieve_body(wp_remote_get($request_uri)));
 
-        $this->github_response = $response;
-    }
+		if (is_array($response)) {
+			return false;
+		}
 
-    public function modify_transient($transient) {
-        if (!isset($transient->checked)) {
-            return $transient;
-        }
+		$this->github_response = $response;
+	}
 
-        $this->get_repository_info();
-        $this->plugin_data = get_plugin_data($this->plugin_file);
+	public function modify_transient($transient)
+	{
+		if (!isset($transient->checked)) {
+			return $transient;
+		}
 
-        $out_of_date = version_compare(
-            str_replace('v', '', $this->github_response->tag_name),
-            $this->plugin_data['Version'],
-            'gt'
-        );
+		$this->get_repository_info();
+		$this->plugin_data = get_plugin_data($this->plugin_file);
 
-        if ($out_of_date) {
-            $package = $this->github_response->zipball_url;
+		$out_of_date = version_compare(
+			str_replace('v', '', $this->github_response->tag_name),
+			$this->plugin_data['Version'],
+			'gt'
+		);
 
-            $new_data = [
-                'slug' => $this->slug,
-                'plugin' => $this->slug,
-                'new_version' => str_replace('v', '', $this->github_response->tag_name),
-                'url' => $this->plugin_data['PluginURI'],
-                'package' => $package,
-            ];
+		if ($out_of_date) {
+			$package = $this->github_response->zipball_url;
 
-            $transient->response[$this->slug] = (object) $new_data;
-        }
+			$new_data = [
+				'slug' => $this->slug,
+				'plugin' => $this->slug,
+				'new_version' => str_replace('v', '', $this->github_response->tag_name),
+				'url' => $this->plugin_data['PluginURI'],
+				'package' => $package,
+			];
 
-        return $transient;
-    }
+			$transient->response[$this->slug] = (object)$new_data;
+		}
 
-    public function plugin_popup($result, $action, $args) {
-        if ($action !== 'plugin_information') {
-            return $result;
-        }
+		return $transient;
+	}
 
-        if (!isset($args->slug) || $args->slug !== $this->slug) {
-            return $result;
-        }
+	public function plugin_popup($result, $action, $args)
+	{
+		if ($action !== 'plugin_information') {
+			return $result;
+		}
 
-        $this->get_repository_info();
+		if (!isset($args->slug) || $args->slug !== $this->slug) {
+			return $result;
+		}
 
-        return (object) [
-            'name' => $this->plugin_data['Name'],
-            'slug' => $this->slug,
-            'version' => str_replace('v', '', $this->github_response->tag_name),
-            'author' => $this->plugin_data['AuthorName'],
-            'author_profile' => $this->plugin_data['AuthorURI'],
-            'last_updated' => $this->github_response->published_at,
-            'homepage' => $this->plugin_data['PluginURI'],
-            'short_description' => $this->plugin_data['Description'],
-            'sections' => [
-                'Description' => $this->plugin_data['Description'],
-                'Updates' => $this->github_response->body,
-            ],
-            'download_link' => $this->github_response->zipball_url
-        ];
-    }
+		$this->get_repository_info();
 
-    public function after_install($response, $hook_extra, $result) {
-        global $wp_filesystem;
+		return (object)[
+			'name' => $this->plugin_data['Name'],
+			'slug' => $this->slug,
+			'version' => str_replace('v', '', $this->github_response->tag_name),
+			'author' => $this->plugin_data['AuthorName'],
+			'author_profile' => $this->plugin_data['AuthorURI'],
+			'last_updated' => $this->github_response->published_at,
+			'homepage' => $this->plugin_data['PluginURI'],
+			'short_description' => $this->plugin_data['Description'],
+			'sections' => [
+				'Description' => $this->plugin_data['Description'],
+				'Updates' => $this->github_response->body,
+			],
+			'download_link' => $this->github_response->zipball_url
+		];
+	}
 
-        $plugin_folder = WP_PLUGIN_DIR . '/' . dirname($this->slug);
-        $wp_filesystem->move($result['destination'], $plugin_folder);
-        $result['destination'] = $plugin_folder;
+	public function after_install($response, $hook_extra, $result)
+	{
+		global $wp_filesystem;
 
-        return $result;
-    }
+		$plugin_folder = WP_PLUGIN_DIR . '/' . dirname($this->slug);
+		$wp_filesystem->move($result['destination'], $plugin_folder);
+		$result['destination'] = $plugin_folder;
+
+		return $result;
+	}
 }
